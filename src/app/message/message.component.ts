830812a0ad6b5510b8c51a5febeb18a49ce63176
Rohil Khakhar <rkkhakhar@myseneca.ca>
2020-06-12 21:36:51 -0400

    commit

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -7,8 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessageComponent implements OnInit {
 
-  constructor() { }
+  id : number;
+  newMessage : string;
+  messageFile : string;
+  messageStatus : string;
+  messageTime : Date = new Date(Date.now());
+  
+  
 
+  constructor() {}
+    
   ngOnInit() {
   }
 
