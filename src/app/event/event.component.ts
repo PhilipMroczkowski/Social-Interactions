fed130e131f04aec70f71b1f5a7517f74a431e88
Rohil Khakhar <rkkhakhar@myseneca.ca>
2020-06-12 21:02:15 -0400

    commit

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event.component.css']
 })
 export class EventComponent implements OnInit {
+  
+  id: number;
+  eventName: string;
+  description: string;
+  location: string;
+  eventDate : Date = new Date();
+  eventDuration : number;
+  eventCategory : string;
+  eventSubcategory : string;
+  host : string;
+  enableRating: boolean;
+  numAttendees : number;
 
   constructor() { }
 
