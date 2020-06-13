74f82ddae4e6e7a88ff3bddf1bba448be04e8f4d
Rohil Khakhar <rkkhakhar@myseneca.ca>
2020-06-12 20:54:49 -0400

    commit

--- src/app/confirmaccount/confirmaccount.component.ts
+++ src/app/confirmaccount/confirmaccount.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfirmaccountComponent implements OnInit {
 
+  userName : string;
+  password: string;
+
   constructor() { }
 
   ngOnInit() {
