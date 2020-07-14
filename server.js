7f7956802b25897e1ae2f60df934a94414fbeba8
Ray <32021785+fluentinstroll@users.noreply.github.com>
2020-07-14 16:28:58 -0400

    messing with route

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/my-app/'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/my-app/index.html'));
+res.sendFile(path.join(__dirname+'/src/app/app.component.html'));
 });
 
 // Start the app by listening on the default Heroku port
