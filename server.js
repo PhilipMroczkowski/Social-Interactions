d3fe92db22e6e1a493cb67c596dc0beba13a0061
Ray <32021785+fluentinstroll@users.noreply.github.com>
2020-07-14 16:44:52 -0400

    changing things

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/social-interactions'));
 
-app.get('/*', function(req,res) {
+app.get('/', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/social-interactions/index.html'));
 });
