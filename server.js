501c775b84290680a1bfe8533af54efad925b770
PhilipMroczkowski <philip.mroczkowski@yahoo.com>
2020-07-14 22:45:03 -0400

    Heroku Routes

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var routes = require("./routes");
 
-const app = express();
+var app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/social-interactions'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/social-interactions/index.html'));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use("/api",routes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/group.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+
+module.exports = app;
