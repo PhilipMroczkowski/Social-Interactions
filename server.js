d57a4f1e25ef72881f183deac6bd8de3c30d595a
PhilipMroczkowski <philip.mroczkowski@yahoo.com>
2020-07-16 16:20:28 -0400

    Updates to deploy on heroku

--- server.js
+++ server.js
@@ -1,25 +1,16 @@
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
-var routes = require("./routes");
+//Install express server
+const express = require('express');
+const path = require('path');
 
-var app = express();
+const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/social-interactions'));
 
-app.use(express.static(path.join(__dirname, 'dist')));
-
-app.use("/api",routes);
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/group.html'));
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/social-interaction/app.component.html'));
 });
 
-// Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
-
-module.exports = app;
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
