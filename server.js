4cf45431d1c59c370a150df6bbcb3e778ad320de
PhilipMroczkowski <philip.mroczkowski@yahoo.com>
2020-07-16 14:41:39 -0400

    Heroku Fixes

--- server.js
+++ server.js
@@ -8,18 +8,12 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-<<<<<<< HEAD
-app.get('/', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/social-interactions/index.html'));
-=======
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use("/api",routes);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/group.html'));
->>>>>>> bc4c6dbb6f05e1fc558f1ae83c27f5dd7cf4285a
 });
 
 // Initialize the app.
