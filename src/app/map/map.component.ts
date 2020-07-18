ccf6eac7b54d43c0806132691f9a25fc7374da36
THlu1999 <luthienhao99@gmail.com>
2020-07-18 19:36:59 -0400

    Commit

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-map',
@@ -6,10 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-
-  
-  constructor() { }
-
+ 
   zoom = 15;
   center: google.maps.LatLngLiteral;
   options: google.maps.MapOptions = {
@@ -20,13 +18,29 @@ export class MapComponent implements OnInit {
     maxZoom: 18,
     minZoom: 10,
   };
+  markerPosition;
+  location = {lat: 0, lng: 0};
+  address = "70 The Pond Road CA";
+
+  constructor() { 
+   
+  }
 
   ngOnInit() {
-    navigator.geolocation.getCurrentPosition(position => {
-      this.center = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude,
-      }
-    })
+    $.get( "https://maps.googleapis.com/maps/api/geocode/json",
+    {address: this.address,
+    key: 'AIzaSyCjzt3sPINQt8OMeLnIFOnHUWJFeLjyx20'},
+  function(data) {
+  console.log(data.results[0].geometry.location.lat);
+
+  this.center = {
+    lat: data.results[0].geometry.location.lat,
+    lng: data.results[0].geometry.location.lng,
+  };
+  this.markerPosition = {
+    lat: data.results[0].geometry.location.lat,
+    lng: data.results[0].geometry.location.lng,
+  };
+});
   }
 }
