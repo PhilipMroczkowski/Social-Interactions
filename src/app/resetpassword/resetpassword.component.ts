8180b00b4e3ef765d89ebd136c78e7d42c113739
PhilipMroczkowski <philip.mroczkowski@yahoo.com>
2020-07-25 12:49:44 -0400

    declaration error

--- src/app/resetpassword/resetpassword.component.ts
+++ src/app/resetpassword/resetpassword.component.ts
@@ -8,7 +8,7 @@ import { UserComponent } from '../user/user.component';
 })
 export class ResetpasswordComponent implements OnInit {
 
-  newPassword : UserComponent['password'];
+ // newPassword : UserComponent['password'];
 
   constructor() { }
 
