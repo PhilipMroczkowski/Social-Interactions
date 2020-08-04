7d0843e71bee627a3c35e07bf49df08d914835ed
THlu1999 <luthienhao99@gmail.com>
2020-08-04 14:18:51 -0400

    commit

---
+++ src/app/event/event.ts
@@ -0,0 +1,9 @@
+export class Group {
+    id : number;
+    eventName: string;
+    description: string;
+    eventDate: Date;
+    eventLocation: string;
+    organizer : string;
+    participant : string;
+  }
