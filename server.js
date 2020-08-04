01361abf91ddb65db6d7ae950c26102626227c8a
Ray <32021785+fluentinstroll@users.noreply.github.com>
2020-08-03 21:28:23 -0400

    reverted change to serverjs

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/my-app'));
-app.use(cors())
 app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname,'/dist/my-app/index.html'));
