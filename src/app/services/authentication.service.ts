68b0844a88ff695e6e7a0a68cbe2ff4b758848e0
Ray <32021785+fluentinstroll@users.noreply.github.com>
2020-08-04 20:46:32 -0400

    corrected post url for login

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -20,7 +20,7 @@ export class AuthenticationService {
     }
 
     login(username: string, password: string) {
-        return this.http.post<any>(this.apiUrl+`/user/authenticate`, { username, password })
+        return this.http.post<any>(this.apiUrl+`/login`, { username, password })
             .pipe(map(user => {
                 // login successful if there's a jwt token in the response
                 if (user && user.token) {
