4c7d0a745b7c90d5cca1863b22cff7b73135094a
Ray <32021785+fluentinstroll@users.noreply.github.com>
2020-08-05 22:19:51 -0400

    view group with id routes should work

--- src/app/group/group.component.ts
+++ src/app/group/group.component.ts
@@ -13,33 +13,20 @@ export class GroupComponent implements OnInit {
   paramSubscription: any;
   groupSubscription: any;
   group: Group;
-
+  id: number;
   constructor(
     private gs: GroupService,
     private ar: ActivatedRoute
-  ) { 
-    /*this.groupName = 'sample group name';
-    this.tags = ['Gaming', 'Sports', 'Food'];
-    this.groupCategory = 'Playing Games';
-    this.groupSubCategory = 'Social';
-    this.groupAdmin = 'user name';
-    this.groupDescription = 'This is a sample group!';
-    this.groupPhoto = 'assets/img/gamingtemp.jpg';*/
-  }
+  ) { }
 
   ngOnInit() {
-    /*this.paramSubscription = this.ar.params.subscribe(params => {
-      this.group.id = params['id'],
-      function(err) {console.log('unable to get id');
-      }
-    });*/
 
     this.paramSubscription = this.ar.params.subscribe(params => {
-      this.group.id = params['_id'],
+      this.id = params['_id'],
       function(err){ console.log('unable to get id');}
     })
 
-    this.groupSubscription = this.gs.getGroup(this.group.id)
+    this.groupSubscription = this.gs.getGroup(this.id)
     .subscribe(
       group => this.group = group[0],
       function(err) {
